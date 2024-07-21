7367e2728ba259f3558f6caf2f91333fb30111b8
Deepak Ugale <ugale.deepak3010@gmail.com>
2024-07-21 11:11:27 +0530

    Empty Gateway microservice added.

---
+++ Li10_Gateway/src/main/java/com/Gateway/Li910GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Li910GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Li910GatewayApplication.class, args);
+	}
+
+}
